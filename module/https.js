dbd0eecff17a61348b76a84c027d8526ae6263fb
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-06-26 00:36:35 +0300

    work on nasa project

--- module/https.js
+++
@@ -1,8 +0,0 @@
-const { send, read } = require('./internals');
-function makeRequest(url, data) {
-    send(url, data);
-    return read();
-}
-
-const responseData = makeRequest('https://google.com', 'karim');
-console.log(responseData);
