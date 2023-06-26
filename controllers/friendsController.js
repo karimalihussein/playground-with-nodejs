02ee5dbda7a5dcb37a675f1c4f9f391c9d821511
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-06-26 18:04:10 +0300

    🛠️refactor(friendsController) use friend model instead of hard-coded array

--- controllers/friendsController.js
+++ controllers/friendsController.js
@@ -1,38 +1,12 @@
-const friends = [
-  {
-    id: 1,
-    name: "John Doe",
-  },
-  {
-    id: 2,
-    name: "Jane Doe",
-  },
-  {
-    id: 3,
-    name: "John Smith",
-  },
-  {
-    id: 4,
-    name: "Jane Smith",
-  },
-  {
-    id: 5,
-    name: "Florin Pop",
-  },
-  {
-    id: 6,
-    name: "Vladimir Putin",
-  },
-];
-
+const model = require("../models/friend");
 
 function getFriends(req, res) {
-  res.json(friends);
+  res.json(model);
 }
 
 function getFriend(req, res) {
     const id = parseInt(req.params.id);
-    const friend = friends.find((friend) => friend.id === id);
+    const friend = model.find((friend) => friend.id === id);
     if (!friend) {
         return res.status(404).send("Friend not found");
     }
@@ -45,10 +19,10 @@ function postFriend(req, res) {
     }
 
     const newFriend = {
-        id: friends.length + 1,
+        id: model.length + 1,
         name: req.body.name,
     };
-    friends.push(newFriend);
+    model.push(newFriend);
     res.json(newFriend);
 }
 
---
+++ models/friend.js
@@ -0,0 +1,28 @@
+const friends = [
+    {
+      id: 1,
+      name: "John Doe",
+    },
+    {
+      id: 2,
+      name: "Jane Doe",
+    },
+    {
+      id: 3,
+      name: "John Smith",
+    },
+    {
+      id: 4,
+      name: "Jane Smith",
+    },
+    {
+      id: 5,
+      name: "Florin Pop",
+    },
+    {
+      id: 6,
+      name: "Vladimir Putin",
+    },
+  ];
+
+  module.exports = friends;
