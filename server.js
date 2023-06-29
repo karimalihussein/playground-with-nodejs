68deb3c13cae9a0a2d551ca4433948e160d7b86e
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-06-29 20:01:39 +0300

    🚀feat(dependencies) add pm2 ⚙️chore(server) refactor server.js

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
-const cluster = require('cluster');
 const port = 3000;
-const os = require('os');
 
 function delay(duration) {
     const start = Date.now();
@@ -19,15 +17,6 @@ app.get('/timer', (req, res) => {
     delay(9000);
     res.send(`Performances example ${process.pid}`)
 });
-console.log('Running server.js..')
-if(cluster.isMaster) {
-    console.log(`Master ${process.pid} is running`);
-    const NUM_workers = os.cpus().length;
-    for (let i = 0; i < NUM_workers; i++) {
-        cluster.fork();
-    }
-} else {
-    console.log(`Worker ${process.pid} is running`);
-    app.listen(port);
-}
+console.log('Running server.js..');
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
