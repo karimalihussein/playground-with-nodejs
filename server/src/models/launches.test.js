fe8f09f92895bac2ed660fcf71313f5fc25ef0f3
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-06-29 04:43:03 +0300

    🚀feat(launches) add test for POST /launches 🛠️refactor(launches) improve error handling in POST /launches ⚙️chore(dependencies) upgrade jest to 27.1.0

--- server/src/models/launches.test.js
+++ server/src/models/launches.test.js
@@ -11,17 +11,67 @@ describe("Test Get /launches", () => {
 });
 
 describe("Test POST /launches", () => {
+  const data = {
+    mission: "Test Mission",
+    rocket: "rocket 1",
+    target: "target 1",
+    launchDate: "2020-01-01",
+    destination: "200km",
+  };
+  const dataWithoutDate = {
+    mission: "Test Mission",
+    rocket: "rocket 1",
+    target: "target 1",
+    destination: "200km",
+  };
   test("It should respond with 201 created", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send(data)
+      .expect("Content-Type", /json/)
+      .expect(201);
+
+    const requestDate = data.launchDate;
+    const responseDate = response.body.launchDate;
+    expect(response.body).toMatchObject(dataWithoutDate);
+    expect(new Date(responseDate)).toEqual(new Date(requestDate));
+  });
+
+  test("It should catch missing required properties", async () => {
+    const response = await request(app)
+      .post("/launches")
+      .send({})
+      .expect("Content-Type", /json/)
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: "Missing required launch property",
+    });
+    
+  });
+
+  test("It should catch invalid dates", async () => {
     const response = await request(app)
       .post("/launches")
       .send({
         mission: "Test Mission",
         rocket: "rocket 1",
         target: "target 1",
-        launchDate: "2020-01-01",
+        launchDate: "not a date",
         destination: "200km",
       })
       .expect("Content-Type", /json/)
-      .expect(201);
+      .expect(400);
+
+    expect(response.body).toStrictEqual({
+      error: "Invalid launch date",
+    });
   });
+
+
 });
+
+
+
+
+
