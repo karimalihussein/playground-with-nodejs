f9b33385159d6dfa73aea26bc042c6501771ab4d
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-07-01 01:40:47 +0300

    🚀feat(docs) add type of errors and why to use Express Js ⚙️chore(dependencies) add nodemon to devDependencies

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My First Page</title></head>');
-    res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-    res.write('</html>');
-    res.end();
+    if(req.url === '/') {
+        res.write('<html>');
+        res.write('<head><title>My first page</title></head>');
+        res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+        res.write('</html>');
+        return res.end(); 
+    }
+
+    process.exit(1); // soft exit the event loop
 });
 server.listen(3000); 
