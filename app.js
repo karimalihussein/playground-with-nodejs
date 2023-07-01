cf88aff98aa844825a294cffec23decdcea7c71b
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-07-01 20:30:52 +0300

    🚀feat(middleware) add middleware functionality 🛠️refactor(server) use express framework ⚙️chore(dependencies) add express dependency

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const http = require('http');
-const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    if(req.url === '/') {
-        res.write('<html>');
-        res.write('<head><title>My first page</title></head>');
-        res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
-        res.write('</html>');
-        return res.end(); 
-    }
-
-    process.exit(1); // soft exit the event loop
+const express = require('express');
+const app = express();
+app.use((req, res, next) => {
+    console.log('In the middleware!');
+    next(); // Allows the request to continue to the next middleware in line
 });
-server.listen(3000); 
+
+app.use((req, res, next) => {
+    console.log('In another middleware!');
+    res.send('<h1>Hello from Express!</h1>'); // Send a response
+});
+
+
+const server = http.createServer(app);
+server.listen(3000, () => {
+    console.log('Server is running...');
+});
