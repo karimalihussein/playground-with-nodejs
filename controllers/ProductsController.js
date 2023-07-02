1e3d65a1c16012aa99e1af335ecd68f2e0f87cd9
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-07-03 00:24:59 +0300

    add new model for products and make sure the controller use this model

--- controllers/ProductsController.js
+++ controllers/ProductsController.js
@@ -1,4 +1,4 @@
-const products = [];
+const ProductModel = require('../models/ProductModel.js');
 
 exports.getAddProduct = (req, res, next) => {
     res.render('products/create', { pageTitle: 'Add Product', path: '/products/create' });
@@ -6,10 +6,11 @@ exports.getAddProduct = (req, res, next) => {
 
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
+    const product = new ProductModel(req.body.title);
+    product.save();
     res.redirect('/products');
 };
 
 exports.getProducts = (req, res, next) => {
-    res.render('products/index', { products, title: 'Shop', path: '/' });
+    res.render('products/index', { products: ProductModel.fetchAll(), title: 'Shop', path: '/' });
 };
