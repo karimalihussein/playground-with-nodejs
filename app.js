a4ea3efb00ab06e767f705fabae6b7bb1ce1744f
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-07-04 02:03:09 +0300

    🚀feat(app) add MongoDB connection ⚙️chore(package.json) upgrade mongoose to 7.3.1

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const courses = require('./routes/courses');
 const instructors = require('./routes/instructors');
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/mydb').then(() => console.log('Connected to MongoDB...')).catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
 app.use('/api/courses', courses);
