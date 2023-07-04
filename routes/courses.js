540e00f9671e90208d0853949eed16d1d57c5e96
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-07-04 12:27:13 +0300

    🚀feat(routes) add CRUD operations for courses and instructors 🛠️refactor(routes) simplify route definitions for courses and instructors

--- routes/courses.js
+++ routes/courses.js
@@ -3,56 +3,9 @@ const router = express.Router();
 const { verifyTokenAndAdmin } = require("../middlewares/VerifyToken");
 const CourseController = require("../controllers/CourseController");
 
-/**
- * @desc: Get all courses
- * @route: GET /api/courses
- * @access: Public
- * @param: req, res
- * @return: courses
- * @method: GET
- */
-router.get('/', CourseController.getAllCourses);
 
-/**
- * @desc: Get a course by id
- * @route: GET /api/courses/:id
- * @access: Public
- * @param: req, res
- * @return: course
- * @method: GET
- */
-router.get('/:id', CourseController.getCourseById);
+router.route('/').get(CourseController.getAllCourses).post(verifyTokenAndAdmin, CourseController.createCourse);
+router.route('/:id').get(CourseController.getCourseById).put(verifyTokenAndAdmin, CourseController.updateCourse).delete(verifyTokenAndAdmin, CourseController.deleteCourse);
 
-/**
- * @desc: Create a course
- * @route: POST /api/courses
- * @access: Private
- * @param: req, res
- * @return: course
- * @method: POST
- */
-router.post('/', verifyTokenAndAdmin, CourseController.createCourse);
- 
-
-/**
- * @desc: Update a course
- * @route: PUT /api/courses/:id
- * @access: Private
- * @param: req, res
- * @return: course
- * @method: PUT
- */
-router.put('/:id', verifyTokenAndAdmin, CourseController.updateCourse);
- 
-
-/**
- * @desc: Delete a course
- * @route: DELETE /api/courses/:id
- * @access: Private
- * @param: req, res
- * @return: course
- * @method: DELETE
- */
-router.delete('/:id', verifyTokenAndAdmin, CourseController.deleteCourse);
 
 module.exports = router;
--- routes/instructors.js
+++ routes/instructors.js
@@ -3,57 +3,8 @@ const router = express.Router();
 const { verifyTokenAndAdmin } = require("../middlewares/VerifyToken");
 const  InstructorController = require("../controllers/InstructorController");
 
-/**
- * @desc: Get all instructors
- * @route: GET /api/instructors
- * @access: Public
- * @param: req, res
- * @return: instructors
- * @method: GET
- */
-router.get('/', InstructorController.getAllInstructors);
+router.route('/').get(InstructorController.getAllInstructors).post(verifyTokenAndAdmin, InstructorController.storeInstructor);
+router.route('/:id').get(InstructorController.getInstructorById).put(verifyTokenAndAdmin, InstructorController.updateInstructor).delete(verifyTokenAndAdmin, InstructorController.deleteInstructor);
 
-/**
- * @desc: Get an instructor by id
- * @route: GET /api/instructors/:id
- * @access: Public
- * @param: req, res
- * @return: instructor
- * @method: GET
- */
-router.get('/:id', InstructorController.getInstructorById);
-  
-
-/**
- * @desc: Create a new instructor
- * @route: POST /api/instructors
- * @access: Private
- * @param: req, res
- * @return: instructor
- * @method: POST
- * @body: name, description
- */
-router.post('/', verifyTokenAndAdmin, InstructorController.storeInstructor);
-
-/**
- * @desc: Update an instructor
- * @route: PUT /api/instructors/:id
- * @access: Private
- * @param: req, res
- * @return: instructor
- * @method: PUT
- * @body: name, description
- */
-router.put('/:id', verifyTokenAndAdmin, InstructorController.updateInstructor);
-
-/**
- * @desc: Delete an instructor
- * @route: DELETE /api/instructors/:id
- * @access: Private
- * @param: req, res
- * @return: instructor
- * @method: DELETE
- */
-router.delete('/:id', verifyTokenAndAdmin, InstructorController.deleteInstructor);
 
 module.exports = router;
