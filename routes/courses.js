d8a745a418104954645e10fd8fe7df0c87a22266
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-07-04 06:05:40 +0300

    ⚙️chore(courses) modify course routes to populate instructor info
    The git diff shows changes made to the "routes/courses.js" file. In summary, the changes include:
    
    - Removing the hardcoded array of courses
    - Modifying the GET /api/courses route to fetch courses from the database using Course.find() and populating the "instructor" field with the "firstName", "lastName", and "image" properties
    - Modifying the GET /api/courses/:id route to fetch a specific course from the database using Course.findById() and populating the "instructor" field with the "firstName", "lastName", and "image" properties
    
    Based on these changes, a possible conventional commit message could be:
    
    ⚙️chore(courses) modify course routes to populate instructor info

--- routes/courses.js
+++ routes/courses.js
@@ -8,40 +8,6 @@ const {
 const asyncHandler = require("express-async-handler");
 const { Course } = require("../models/Course");
 
-const courses = [
-  {
-    id: 1,
-    name: "Javascript Course From Zero to Hero",
-    description:
-      "This course is for beginners who want to learn javascript from scratch",
-    price: 100,
-    duration: "2 months",
-    instructor: "John Doe",
-    rating: 4.5,
-    category: "Programming",
-  },
-  {
-    id: 2,
-    name: "Python Course From Zero to Hero",
-    description:
-      "This course is for beginners who want to learn python from scratch",
-    price: 140,
-    duration: "3 months",
-    instructor: "Maxmillian Schwarzmuller",
-    rating: 4.8,
-    category: "Programming",
-  },
-  {
-    id: 3,
-    name: "React Course From Zero to Hero",
-    description:
-      "This course is for beginners who want to learn react from scratch",
-    price: 120,
-    duration: "1 months",
-    instructor: "John Doe",
-  },
-];
-
 /**
  * @desc: Get all courses
  * @route: GET /api/courses
@@ -53,7 +19,10 @@ const courses = [
 router.get(
   "/",
   asyncHandler(async (req, res) => {
-    const courses = await Course.find();
+    const courses = await Course.find().populate(
+      "instructor",
+      "firstName lastName image"
+    );
     res.status(200).json(courses);
   })
 );
@@ -69,7 +38,10 @@ router.get(
 router.get(
   "/:id",
   asyncHandler(async (req, res) => {
-    const course = await Course.findById(req.params.id);
+    const course = await Course.findById(req.params.id).populate(
+      "instructor",
+      "firstName lastName image"
+    );
     if (!course) {
       res.status(404).send("The course with the given ID was not found.");
       return;
