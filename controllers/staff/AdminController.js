443b10ad4d874043beeb331bfcb34180be368e25
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-07-06 03:21:22 +0300

    🚀feat(admin) add registerAdmin, loginAdmin, getAllAdmins,AdminById, updateAdmin, deleteAdmin, suspendTeacher unsuspendTeacher, withdrawTeacher, unwithdrawTeacher functions to AdminController 🛠️refactor(adminRouter) refactor routes to use functions from AdminController

---
+++ controllers/staff/AdminController.js
@@ -0,0 +1,201 @@
+
+/**
+ * @description: Register admin 
+ * @route: POST /api/admins/register
+ * @access: Private
+ */
+const registerAdmin =  (req, res) => {
+    try {
+        res.status(200).json({
+            message: 'Admin registered successfully!',
+            status: 'success'
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Internal server error!',
+            status: 'error'
+        });
+    }
+};
+
+/**
+ * @description: Login admin
+ * @route: POST /api/admins/login
+ * @access: Public
+ */
+const loginAdmin = (req, res) => {
+    try {
+        res.status(200).json({
+            message: 'Admin logged in successfully!',
+            status: 'success'
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Internal server error!',
+            status: 'error'
+        });
+    }
+};
+
+/**
+ * @description: Get all admins
+ * @route: GET /api/admins
+ * @access: Private
+ */
+const getAllAdmins = (req, res) => {
+    try {
+        res.status(200).json({
+            message: 'Admins fetched successfully!',
+            status: 'success'
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Internal server error!',
+            status: 'error'
+        });
+    }
+}
+
+/**
+ * @description: Get admin by id
+ * @route: GET /api/admins/:id
+ * @access: Private
+*/
+const getAdminById = (req, res) => {
+    try {
+        res.status(200).json({
+            message: 'Admin fetched successfully!',
+            status: 'success'
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Internal server error!',
+            status: 'error'
+        }); 
+    }
+};
+
+/**
+ * @description: Update admin
+ * @route: PUT /api/admins/:id
+ * @access: Private
+ */
+const updateAdmin = (req, res) => {
+    try {
+        res.status(200).json({
+            message: 'Admin updated successfully!',
+            status: 'success'
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Internal server error!',
+            status: 'error'
+        }); 
+    }
+};
+
+/**
+ * @description: Delete admin
+ * @route: DELETE /api/admins/:id
+ * @access: Private
+ */
+const deleteAdmin = (req, res) => {
+    try {
+        res.status(200).json({
+            message: 'Admin deleted successfully!',
+            status: 'success'
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Internal server error!',
+            status: 'error'
+        }); 
+    }
+};
+
+/**
+ * @description: suspend a teacher
+ * @route: PUT /api/admins/suspend/teacher/:id
+ * @access: Private
+ */
+const suspendTeacher = (req, res) => {
+    try {
+        res.status(200).json({
+            message: 'Teacher suspended successfully!',
+            status: 'success'
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Internal server error!',
+            status: 'error'
+        }); 
+    }
+};
+
+/**
+ * @description: unsuspend a teacher
+ * @route: PUT /api/admins/unsuspend/teacher/:id
+ * @access: Private
+ */
+const unsuspendTeacher = (req, res) => {
+    try {
+        res.status(200).json({
+            message: 'Teacher unsuspended successfully!',
+            status: 'success'
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Internal server error!',
+            status: 'error'
+        }); 
+    }
+};
+
+/**
+ * @description: withdraw a teacher
+ * @route: PUT /api/admins/withdraw/teacher/:id
+ * @access: Private
+ */
+const withdrawTeacher = (req, res) => {
+    try {
+        res.status(200).json({
+            message: 'Teacher withdrawn successfully!',
+            status: 'success'
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Internal server error!',
+            status: 'error'
+        }); 
+    }
+};
+
+/**
+ * @description: unwithdraw a teacher
+ * @route: PUT /api/admins/unwithdraw/teacher/:id
+ * @access: Private
+ */
+const unwithdrawTeacher = (req, res) => {
+    try {
+        res.status(200).json({
+            message: 'Teacher unwithdrawn successfully!',
+            status: 'success'
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: 'Internal server error!',
+            status: 'error'
+        }); 
+    }
+};
+
+
+
+module.exports = { registerAdmin, loginAdmin, getAllAdmins, getAdminById, updateAdmin, deleteAdmin, suspendTeacher, unsuspendTeacher, withdrawTeacher, unwithdrawTeacher }
+
+
+
+
+
+
+
--- routes/staff/adminRouter.js
+++ routes/staff/adminRouter.js
@@ -1,165 +1,18 @@
 const express = require('express');
 const adminRouter = express.Router();
+const  AdminController  = require('../../controllers/staff/AdminController');
 
-adminRouter.post('/register', (req, res) => {
-    try {
-        res.status(200).json({
-            message: 'Admin registered successfully!',
-            status: 'success'
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: 'Internal server error!',
-            status: 'error'
-        });
-    }
-});
-
-adminRouter.post('/login', (req, res) => {
-    try {
-        res.status(200).json({
-            message: 'Admin logged in successfully!',
-            status: 'success'
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: 'Internal server error!',
-            status: 'error'
-        });
-    }
-});
-
-adminRouter.post('/', (req, res) => {
-    try {
-        res.status(200).json({
-            message: 'Admin created successfully!',
-            status: 'success'
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: 'Internal server error!',
-            status: 'error'
-        });
-    }
-});
-
-
-adminRouter.get('/', (req, res) => {
-    try {
-        res.status(200).json({
-            message: 'Admins fetched successfully!',
-            status: 'success'
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: 'Internal server error!',
-            status: 'error'
-        });
-    }
-});
-
-
-
-adminRouter.get('/:id', (req, res) => {
-    try {
-        res.status(200).json({
-            message: 'Admin fetched successfully!',
-            status: 'success'
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: 'Internal server error!',
-            status: 'error'
-        }); 
-    }
-  
-});
-
-adminRouter.put('/:id', (req, res) => {
-    try {
-        res.status(200).json({
-            message: 'Admin updated successfully!',
-            status: 'success'
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: 'Internal server error!',
-            status: 'error'
-        }); 
-    }
-  
-});
-
-adminRouter.delete('/:id', (req, res) => {
-    try {
-        res.status(200).json({
-            message: 'Admin deleted successfully!',
-            status: 'success'
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: 'Internal server error!',
-            status: 'error'
-        }); 
-    }
-});
-
-adminRouter.put('/suspend/teacher/:id', (req, res) => {
-    try {
-        res.status(200).json({
-            message: 'Teacher suspended successfully!',
-            status: 'success'
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: 'Internal server error!',
-            status: 'error'
-        }); 
-    }
-});
-
-
-adminRouter.put('/unsuspend/teacher/:id', (req, res) => {
-    try {
-        res.status(200).json({
-            message: 'Teacher unsuspended successfully!',
-            status: 'success'
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: 'Internal server error!',
-            status: 'error'
-        }); 
-    }
-});
-
-adminRouter.put('/withdraw/teacher/:id', (req, res) => {
-    try {
-        res.status(200).json({
-            message: 'Teacher withdrawn successfully!',
-            status: 'success'
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: 'Internal server error!',
-            status: 'error'
-        }); 
-    }
-});
-
-adminRouter.put('/unwithdraw/teacher/:id', (req, res) => {
-    try {
-        res.status(200).json({
-            message: 'Teacher unwithdrawn successfully!',
-            status: 'success'
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: 'Internal server error!',
-            status: 'error'
-        }); 
-    }
-});
+adminRouter.post('/register', AdminController.registerAdmin);
+adminRouter.post('/login', AdminController.loginAdmin);
+adminRouter.post('/', AdminController.getAllAdmins);
+adminRouter.get('/', AdminController.getAllAdmins);
+adminRouter.get('/:id', AdminController.getAdminById);
+adminRouter.put('/:id', AdminController.updateAdmin);
+adminRouter.delete('/:id', AdminController.deleteAdmin);
+adminRouter.put('/suspend/teacher/:id', AdminController.suspendTeacher);
+adminRouter.put('/unsuspend/teacher/:id', AdminController.unsuspendTeacher);
+adminRouter.put('/withdraw/teacher/:id', AdminController.withdrawTeacher);
+adminRouter.put('/unwithdraw/teacher/:id', AdminController.unwithdrawTeacher);
 
 
 module.exports = adminRouter;
