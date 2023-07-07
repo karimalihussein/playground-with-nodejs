a9f6cb48d6cfb7f454043c565c97f8738783563c
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-07-08 02:48:46 +0300

    🚀feat(academic) add AcademicTermController 🚀feat(academic) add AcademicTermRoutes ⚙️chore(academic) update AcademicYear routes

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const adminRouter = require('../routes/staff/adminRouter');
 const AcademicYearRouter = require('../routes/academics/academicYear');
+const AcademicTermRouter = require('../routes/academics/academicTerm');
 const GlobalErrorHandlerMiddleware = require('../middlewares/GlobalErrorHandlerMiddleware');
 
 const app = express();
@@ -13,7 +14,7 @@ app.use(express.json()); // pass incoming json data to req.body
 // Routes
 app.use('/api/v1/admins', adminRouter);
 app.use('/api/v1/academic-years', AcademicYearRouter);
-
+app.use('/api/v1/academic-terms', AcademicTermRouter);
 // Error handling
 app.use(GlobalErrorHandlerMiddleware);
 
