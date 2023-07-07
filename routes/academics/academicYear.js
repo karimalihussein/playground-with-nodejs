3f2cb6f87c6de547607dfd7b97a949d016f7744e
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-07-08 02:15:43 +0300

    work on routes chaining

--- routes/academics/academicYear.js
+++ routes/academics/academicYear.js
@@ -4,10 +4,13 @@ const AcademicYearController = require('../../controllers/academics/AcademicYear
 const IsLogin = require('../../middlewares/LoginMiddleware');
 const isAdmin = require('../../middlewares/AdminMiddleware');
 
-AcademicYearRouter.post('/', IsLogin, isAdmin, AcademicYearController.createAcademicYear);
-AcademicYearRouter.get('/', IsLogin, isAdmin, AcademicYearController.getAcademicYears);
-AcademicYearRouter.get('/:id', IsLogin, isAdmin, AcademicYearController.getAcademicYearById);
-AcademicYearRouter.put('/:id', IsLogin, isAdmin, AcademicYearController.updateAcademicYear);
-AcademicYearRouter.delete('/:id', IsLogin, isAdmin, AcademicYearController.deleteAcademicYear);
+AcademicYearRouter.route('/').post(IsLogin, isAdmin).get(IsLogin, isAdmin);
+AcademicYearRouter.route('/:id').get(IsLogin, isAdmin).put(IsLogin, isAdmin).delete(IsLogin, isAdmin);
+
+// AcademicYearRouter.post('/', IsLogin, isAdmin, AcademicYearController.createAcademicYear);
+// AcademicYearRouter.get('/', IsLogin, isAdmin, AcademicYearController.getAcademicYears);
+// AcademicYearRouter.get('/:id', IsLogin, isAdmin, AcademicYearController.getAcademicYearById);
+// AcademicYearRouter.put('/:id', IsLogin, isAdmin, AcademicYearController.updateAcademicYear);
+// AcademicYearRouter.delete('/:id', IsLogin, isAdmin, AcademicYearController.deleteAcademicYear);
 
 module.exports = AcademicYearRouter;
