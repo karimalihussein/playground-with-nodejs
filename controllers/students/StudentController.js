b8e7e07549a0b8ace2ec1dfee547db09c4de1776
Karim ALi Hussein <karimalihussein99@gmail.com>
2023-07-09 02:18:20 +0300

    🚀feat(student) add update your profile functionality
    🛠️refactor(student) update student profile updating functionality

--- controllers/students/StudentController.js
+++ controllers/students/StudentController.js
@@ -40,6 +40,27 @@ const getYourOwnProfile = AysncHandler(async (req, res) => {
   });
 });
 
+const updateYourProfile = AysncHandler(async (req, res) => {
+  const { name, email, password, age, phone } = req.body;
+  if (password) {
+    const salt = await bcrypt.genSalt(10);
+    password = await bcrypt.hash(password, salt);
+  }
+  const student = await Student.findByIdAndUpdate(req.userAuth._id, { 
+    name, 
+    email, 
+    password,
+    age, 
+    phone 
+  }, { new: true }).select('-password -createdAt -updatedAt -__v');
+  if (!student) { return res.json({ message: "Student does not exist!" });};
+  res.status(200).json({
+    message: "Your profile updated successfully!",
+    status: "success",
+    data: student,
+  });
+});
+
 const index = AysncHandler(async (req, res) => {
   const students = await Student.find({}).select('-password -createdAt -updatedAt -__v');
   res.status(200).json({
@@ -54,9 +75,58 @@ const show = AysncHandler(async (req, res) => {
   const student = await Student.findById(id).select('-password -createdAt -updatedAt -__v');
   if (!student) { return res.json({ message: "Student does not exist!" });};
   res.status(200).json({
-    message: "Student profile fetched successfully!",
+    message: "Student fetched successfully!",
     status: "success",
     data: student,
+  });
+});
+
+const update = AysncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { classLevel, academicYear, program, name, email, phone, prefectName } = req.body;
+
+  const student = await Student.findById(id);
+
+  if (!student) {
+    return res.json({ message: "Student does not exist!" });
+  }
+
+  if (email) {
+    const studentFound = await Student.findOne({ email });
+    if (studentFound && studentFound._id != id) {
+      return res.json({ message: "Student already exists!" });
+    }
+  }
+
+  if (phone) {
+    const studentPhoneFound = await Student.findOne({ phone });
+    if (studentPhoneFound && studentPhoneFound._id != id) {
+      return res.json({ message: "Student already exists!" });
+    }
+  }
+
+  const updatedData = {
+    name,
+    email,
+    academicYear,
+    program,
+    prefectName,
+  };
+
+  if (classLevel) {
+    updatedData.classLevels = classLevel;
+  }
+
+  const studentUpdated = await Student.findByIdAndUpdate(
+    id,
+    { $set: updatedData },
+    { new: true }
+  );
+
+  res.status(200).json({
+    message: "Student updated successfully!",
+    status: "success",
+    data: studentUpdated,
   });
 });
 
@@ -66,5 +136,7 @@ module.exports = {
   login,
   getYourOwnProfile,
   index,
-  show
+  show,
+  updateYourProfile,
+  update
 };
--- routes/staff/studentRouter.js
+++ routes/staff/studentRouter.js
@@ -5,11 +5,11 @@ const IsLogin = require("../../middlewares/LoginMiddleware");
 const IsAdmin = require("../../middlewares/AdminMiddleware");
 const StudentMiddleware = require("../../middlewares/StudentMiddleware");
 Router.get("/profile",  StudentMiddleware.isStudentLoggedIn, StudentMiddleware.isStudent, Controller.getYourOwnProfile);
-// Router.put("/profile", TeacherMiddleware.isTeacherLoggedIn, TeacherMiddleware.isTeacher, TeacherController.updateTeacherProfile);
+Router.put("/profile", StudentMiddleware.isStudentLoggedIn, StudentMiddleware.isStudent, Controller.updateYourProfile);
 Router.post("/admin/register", IsLogin, IsAdmin, Controller.register);
 Router.post("/login", Controller.login);
 Router.get("/", IsLogin, IsAdmin, Controller.index);
 Router.get("/:id", IsLogin, IsAdmin, Controller.show);
-// Router.put("/:id", IsLogin, isAdmin, TeacherController.adminUpdateTeacherProfile);
+Router.put("/:id", IsLogin, IsAdmin, Controller.update);
 
 module.exports = Router;
